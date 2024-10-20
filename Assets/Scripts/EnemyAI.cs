12e52251f1b272565dba786451aca7e750ebd28f
Ben-dev-0 <benstefl8@gmail.com>
2024-10-20 13:05:02 -0700

    enemy movment and patrolling

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,87 +1,70 @@
 using System.Collections;
 using UnityEngine;
 
-public class EnemyAI : MonoBehaviour
-{
-    public float speed = 2f;
-    public Transform[] patrolPoints;
+public class EnemyAI : MonoBehaviour {
+    [SerializeField] private float speed = 2f;
+    [SerializeField] private Transform[] patrolPoints;
+    [SerializeField] private float detectionRadius = 5f;
+    [SerializeField] private float stunDuration = 3f;
     private int currentPoint = 0;
-
-    public float detectionRadius = 5f;
-    private Transform player;
+    private Transform playerPos;
     private bool isChasing = false;
-
     private bool isStunned = false;
-    public float stunDuration = 3f;
-
     private Animator animator;
 
-    void Start()
-    {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
+    void Start() {
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform;
         animator = GetComponent<Animator>();
-
         animator.SetBool("IsPatrolling", true);
     }
 
-    void Update()
-    {
+    void Update() {
         if (isStunned) return;
 
-        if (isChasing)
-        {
+        if (isChasing) {
             ChasePlayer();
 
-            if (Vector2.Distance(transform.position, player.position) > detectionRadius)
-            {
+            if (Vector2.Distance(transform.position, playerPos.position) > detectionRadius) {
                 isChasing = false;
                 animator.SetBool("IsChasing", false);
                 animator.SetBool("IsPatrolling", true);
             }
         }
-        else
-        {
+        else {
             Patrol();
             DetectPlayer();
         }
     }
 
-    void Patrol()
-    {
+    void Patrol() {
         animator.SetBool("IsPatrolling", true);
         animator.SetBool("IsChasing", false);
 
         Transform targetPoint = patrolPoints[currentPoint];
         transform.position = Vector2.MoveTowards(transform.position, targetPoint.position, speed * Time.deltaTime);
 
-        if (Vector2.Distance(transform.position, targetPoint.position) < 0.2f)
-        {
+        if (Vector2.Distance(transform.position, targetPoint.position) < 0.2f) {
             currentPoint = (currentPoint + 1) % patrolPoints.Length;
         }
     }
 
-    void DetectPlayer()
-    {
-        if (Vector2.Distance(transform.position, player.position) < detectionRadius)
-        {
+    void DetectPlayer() {
+        if (Vector2.Distance(transform.position, playerPos.position) < detectionRadius) {
             isChasing = true;
             animator.SetBool("IsChasing", true);
             animator.SetBool("IsPatrolling", false);
         }
     }
 
-    void ChasePlayer()
-    {
-        transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+    void ChasePlayer() {
+        transform.position = Vector2.MoveTowards(transform.position, playerPos.position, speed * Time.deltaTime);
     }
 
-    public void StunEnemy()
-    {
+    public void StunEnemy() {
         StartCoroutine(Stunned());
     }
 
-    IEnumerator Stunned()
-    {
+    IEnumerator Stunned() {
         isStunned = true;
         isChasing = false;
         animator.SetBool("IsStunned", true);
@@ -96,11 +79,12 @@ public class EnemyAI : MonoBehaviour
         animator.SetBool("IsPatrolling", true);
     }
 
+    /*
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.CompareTag("Player"))
         {
             collision.collider.GetComponent<HealthManager>().TakeDamage(1);
         }
-    }
+    }*/
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour {
-    [SerializeField] Rigidbody2D rb;
     [SerializeField] float moveSpeed;
-
+    private Rigidbody2D rb;
+    private Animator animator;
+    
     void Start() {
-        
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update() {
         Vector2 direction = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
         direction.Normalize();
         rb.velocity = direction * moveSpeed;
+
+        if (animator != null) {
+            animator.SetFloat("MoveX", direction.x);
+            animator.SetFloat("MoveY", direction.y);
+            animator.SetFloat("Speed", direction.sqrMagnitude);
+
+            if (direction != Vector2.zero) {
+                animator.SetFloat("LastMoveX", direction.x);
+                animator.SetFloat("LastMoveY", direction.y);
+            }
+        }
     }
 }
